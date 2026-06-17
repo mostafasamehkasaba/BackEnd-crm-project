541c97b60a1676c48c1406d394aee5d7ee67ca7f
mostafa <mostafakasaba8@gmail.com>
2026-06-17 12:48:34 +0300

    ..

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -2,24 +2,24 @@ import {Router} from  'express'
 import {registerController,loginController,refreshTokenController, logoutController,verifiyEmailController} from "./auth.controller.js"
 import auth from '../../common/middleware/auth.middleware.js'
 import adminOnly from '../../common/middleware/admin.middleware.js'
-import {registerValidation,
-  loginValidation,
-  refreshTokenValidation,
-  logoutValidation} from "./auth.validation.js"
+// import {registerValidation,
+//   loginValidation,
+//   refreshTokenValidation,
+//   logoutValidation} from "./auth.validation.js"
 
 const router = Router()
 
 
-router.post("/register",registerValidation,registerController)
-router.post("/login",loginValidation,loginController)
-router.post("/refresh-token", refreshTokenValidation,refreshTokenController);
+router.post("/register",registerController)
+router.post("/login",loginController)
+router.post("/refresh-token",refreshTokenController);
 
 router.get("/admin-test", auth, adminOnly, (req, res) => {
   res.json({ message: "Welcome Admin!" })
 })
 
 
-router.post("/logout",logoutValidation, logoutController);
+router.post("/logout", logoutController);
 
 router.get("/verify-email/:token", verifiyEmailController);
 export default router
