2ef65004de757b1cf8b65204278fdaeca643ff5a
mostafa sameh <164693385+mostafasamehkasaba@users.noreply.github.com>
2026-06-20 18:16:22 +0300

    ...

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import { DBconnection } from "./src/DB/connectionDB.js";
 import userRouter from "./src/modules/auth/auth.route.js";
 import propertyRouter from "./src/modules/property/property.route.js";
 import clientRouter from "./src/modules/clients/client.route.js";
-import categoryRouter from "./src/modules/category/category.route.js"
-import invoiceRouter  from "./src/modules/invoices/invoice.route.js"
-import InstallmentRouter from "./src/modules/InstallmentPlan/installment.route.js"
+import categoryRouter from "./src/modules/category/category.route.js";
+import invoiceRouter from "./src/modules/invoices/invoice.route.js";
+import InstallmentRouter from "./src/modules/InstallmentPlan/installment.route.js";
 
 const app = express();
-
 app.use(express.json());
 
-// app.use(express.urlencoded({ extended: true }));
+// ✅ اتصل بالـ DB قبل أي request
+app.use(async (req, res, next) => {
+  await DBconnection();
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("test");
 });
 
-app.use("/api/auth", userRouter);          
-app.use("/api/propertie", propertyRouter); 
-app.use("/api/client", clientRouter);
+app.use("/api/auth", userRouter);
+app.use("/api/properties", propertyRouter);
+app.use("/api/clients", clientRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/invoice", invoiceRouter);
 app.use("/api/installmentPlan", InstallmentRouter);
