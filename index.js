70ccc2ef0f1ff7ca245fe3ecdb448a63348a38b2
mostafa sameh <164693385+mostafasamehkasaba@users.noreply.github.com>
2026-06-21 20:37:59 +0300

    add payment

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ import clientRouter from "./src/modules/clients/client.route.js";
 import categoryRouter from "./src/modules/category/category.route.js";
 import invoiceRouter from "./src/modules/invoices/invoice.route.js";
 import InstallmentRouter from "./src/modules/InstallmentPlan/installment.route.js";
+import paymentRouter from "./src/modules/payment/payment.route.js";
 
 const app = express();
+
+// payment 
+app.use(
+  "/api/payments/webhook",
+  express.raw({ type: "application/json" }),  // raw bytes
+   paymentRouter
+);
 app.use(express.json());
 
 // ✅ اتصل بالـ DB قبل أي request
@@ -17,6 +25,8 @@ app.use(async (req, res, next) => {
   await DBconnection();
   next();
 });
+
+
 
 app.get("/", (req, res) => {
   res.send("test");
@@ -28,5 +38,6 @@ app.use("/api/clients", clientRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/invoice", invoiceRouter);
 app.use("/api/installmentPlan", InstallmentRouter);
+app.use("/api/payments", paymentRouter);
 
 export default app;
---
+++ src/modules/payment/payment.controller.js
@@ -0,0 +1,63 @@
+import * as PaymentService from "./payment.service.js";
+import successResponse from "../../common/responses/successResponse.js";
+import errorResponse from "../../common/responses/errorResponse.js";
+import { HttpStatus } from "../../common/constants/httpStatus.constant.js";
+
+
+export const createCheckoutSessionController = async(req,res) =>{
+    try{
+        const session = await PaymentService.createCheckoutSession(req.body)
+
+        return successResponse(
+            res, "Checkout session created sucessfuly", 
+            session,
+            HttpStatus.CREATED
+        )
+
+    }catch(err){
+     return  errorResponse(res,err.message,HttpStatus.BAD_REQUEST)
+    }
+}
+
+
+export const webhookController = async(req,res)=>{
+    const signature = req.headers["stripe-signature"];
+    try{
+        const result = await PaymentService.handleWebhook(req.body , signature);
+
+        return successResponse(res,HttpStatus.OK,result)
+
+    }catch(err){
+        return errorResponse(res,err.message , HttpStatus.BAD_REQUEST)
+    }
+}
+
+export const getAllPaymentsController = async (req, res) => {
+  try {
+    const payments = await PaymentService.getAllPayments();
+
+    return successResponse(
+      res,
+      "Payments fetched successfully",
+      payments,
+      HttpStatus.OK
+    );
+  } catch (err) {
+    return errorResponse(res, err.message, HttpStatus.BAD_REQUEST);
+  }
+};
+
+export const getPaymentByIdController = async (req, res) => {
+  try {
+    const payment = await PaymentService.getPaymentById(req.params.id);
+
+    return successResponse(
+      res,
+      "Payment fetched successfully",
+      payment,
+      HttpStatus.OK
+    );
+  } catch (err) {
+    return errorResponse(res, err.message, HttpStatus.BAD_REQUEST);
+  }
+};
