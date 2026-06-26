b068f64828cc5c097a0134aeab5494bb0f4097f5
mostafa sameh <164693385+mostafasamehkasaba@users.noreply.github.com>
2026-06-26 23:27:25 +0300

    edit..

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { setServers } from "node:dns/promises";
-setServers(["1.1.1.1", "1.0.0.1"]);
+// import { setServers } from "node:dns/promises";
+// setServers(["1.1.1.1", "1.0.0.1"]);
 
 import app from "./index.js";
 import { DBconnection } from "./src/DB/connectionDB.js";
