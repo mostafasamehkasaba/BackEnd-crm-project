e9a51f5d9c6ee7597a6700caa4a1ef501c0f34a2
mostafa sameh <164693385+mostafasamehkasaba@users.noreply.github.com>
2026-06-26 23:41:24 +0300

    ...

--- src/modules/settings/setting.route.js
+++ src/modules/settings/setting.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import * as companyController from './setting.controller.js';
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ storage: multer.memoryStorage() });
 const router = express.Router();
 
 router.get('/', companyController.getCompany);
