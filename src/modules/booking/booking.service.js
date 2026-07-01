ec65b4c533c0a6719c57119740dffc092aee7152
mostafa sameh <164693385+mostafasamehkasaba@users.noreply.github.com>
2026-07-01 09:20:11 +0300

    edit in booking

--- src/modules/booking/booking.service.js
+++ src/modules/booking/booking.service.js
@@ -2,26 +2,35 @@
 import { bookingModel } from "../../DB/models/booking.model.js";
 import { PropertyModel } from "../../DB/models/property.model.js";
 
-export const createBooking = async (data) => {
-  const { property_id, name, email, phone } = data;
+// modules/booking/booking.service.js
 
- 
+export const createBooking = async (data) => {
+  const { property_id, name, email, phone, status } = data;
+
   const property = await PropertyModel.findById(property_id);
   if (!property) throw new Error("Property not found");
 
-  
-  if (property.status !== "available") {
+  if (status === "CONFIRMED" && property.status !== "available") {
     throw new Error("Property is already booked or unavailable");
   }
+
   const booking = await bookingModel.create({
     property_id,
     name,
     email,
     phone,
-    status
+    status: status || "PENDING",  
   });
-  await property.save()
 
+
+  if (status === "CONFIRMED") {
+    property.status = "booked";
+    await property.save();
+  } 
+  else {
+    property.status = "available";
+    await property.save();
+  }
 
   return booking;
 };
