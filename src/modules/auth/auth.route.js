c364216b936f1d81fcf8493ab70ecde6f3d511c2
mostafa sameh <164693385+mostafasamehkasaba@users.noreply.github.com>
2026-07-02 01:50:41 +0300

    ...

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -2,6 +2,7 @@ import {Router} from  'express'
 import {registerController,loginController,refreshTokenController, logoutController,verifiyEmailController, getUsers} from "./auth.controller.js"
 import auth from '../../common/middleware/auth.middleware.js'
 import adminOnly from '../../common/middleware/admin.middleware.js'
+import { getAllUsers } from './auth.service.js'
 // import {registerValidation,
 //   loginValidation,
 //   refreshTokenValidation,
@@ -18,7 +19,7 @@ router.get("/admin-test", auth, adminOnly, (req, res) => {
   res.json({ message: "Welcome Admin!" })
 })
 
-router.get("/users", getUsers);
+router.get("/users", getAllUsers);
 
 router.post("/logout", logoutController);
 
